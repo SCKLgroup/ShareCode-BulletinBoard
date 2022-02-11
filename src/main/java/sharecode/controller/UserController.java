a9940b8119811ca87d2d2f4c56d0fcb4280da9f4
eukjin <wjchang8028@naver.com>
2022-02-11 15:29:08 +0900

    최종

--- src/main/java/sharecode/controller/UserController.java
+++ src/main/java/sharecode/controller/UserController.java
@@ -13,18 +13,11 @@ public class UserController {
 	@Autowired
 	private UserDao dao;
 	
-<<<<<<< HEAD
 	@RequestMapping(value = "/shareCode/signup.do")
 	public String userSignUpAction(UserVO vo) {
 		System.out.println("컨트롤러 실행");
 		dao.signupUser(vo);
 		return "sharecode/signup_success";
-=======
-	@RequestMapping(value = "/sharecode/signup.do")
-	public String userSignUpAction(UserVO vo) {
-		dao.signupUser(vo);
-		return "";
->>>>>>> branch 'master' of https://github.com/shareCodeProject/shareCode.git
 	}
 
 }
--- src/main/java/sharecode/dao/UserDao.java
+++ src/main/java/sharecode/dao/UserDao.java
@@ -1,6 +1,5 @@
 package sharecode.dao;
 
-<<<<<<< HEAD
 import javax.annotation.Resource;
 import javax.inject.Inject;
 
@@ -24,19 +23,6 @@ public class UserDao extends SqlSessionDaoSupport {
 	public void signupUser(UserVO vo) { //회원정보추가
 		System.out.println("dao실행");
 		this.getSqlSession().insert("signUpUser",vo);
-=======
-import org.mybatis.spring.support.SqlSessionDaoSupport;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Repository;
 
-import sharecode.vo.UserVO;
-
-@Repository
-@ComponentScan(value="sharecode.dao")
-public class UserDao extends SqlSessionDaoSupport {
-	
-	public void signupUser(UserVO vo) { //회원정보추가
-		this.getSqlSession().insert("singUpUser",vo);
->>>>>>> branch 'master' of https://github.com/shareCodeProject/shareCode.git
 	}
 }
